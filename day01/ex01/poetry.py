50fcc5a45689f4f660d975958b359643d8866e0d
mos.dani@yahoo.com <mos.dani@yahoo.com>
2021-09-28 16:37:32 +0000

    Ex01

---
+++ day01/ex01/poetry.py
@@ -0,0 +1,1 @@
+print("Again, I rely on words \n\tThere is no music to break out of anyone's bone \n\t\tNor does the soul have in itself the peace proper to the happy hours \n\tNor the strong friend close to sad and great ideas. \n    And neither is an omniscient old fox")
