7d1c39aa31399567eee94993c58f8080c24619d8
mos.dani@yahoo.com <mos.dani@yahoo.com>
2021-09-29 19:07:09 +0000

    Ex03 revised

--- day01/ex03/count_words.py
+++ day01/ex03/count_words.py
@@ -1,21 +1,12 @@
 def count_words():
-	wordsNumber = 0
-	string = input()
+	
+    string = input()
 
-	index = 0
+    string = string.split()
 
-	while index < len(string) - 1 and string[index] != 4:
+    wordsNumber = len(string)
 
-		while ( string[index] == ' ' or string[index] == '\t') and index < len(string) - 1:
-			index += 1
-
-		if string[index] != ' ' and string[index] != '\t':
-			wordsNumber += 1
-
-		while ( string[index] != ' ' and string[index] != '\t') and index < len(string) - 1:
-			index += 1
-
-	if wordsNumber == 0:
-		raise EOFError('No input provided')
-	else:
-		return wordsNumber
+    if wordsNumber == 0:
+        raise EOFError('No input provided')
+    else:
+        return wordsNumber
