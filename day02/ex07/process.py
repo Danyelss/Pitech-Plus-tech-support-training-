d5632d4972a9d6881a3eefb22bc495556b396ec3
mos.dani@yahoo.com <mos.dani@yahoo.com>
2021-09-30 16:42:45 +0000

    README + Ex07 - Not done

--- day02/ex07/process.py
+++ day02/ex07/process.py
@@ -1,16 +1,40 @@
+import _thread
+import time
+import sys
+import os
+from subprocess import Popen, PIPE
+import subprocess
+
+
+
+def second_thread_function(threadName, minutes):
+    time.sleep(120)
+
+def first_thread_function(threadName, minutes):
+    #process = Popen(['python3', 'process.py', '1', '1', '1', '1'])
+    subprocess.run(["python3", "process.py", '1', '1', '1', '1']) 
+    time.sleep(60 * minutes)
+
+
 def main():
-    try:
-    _thread.start_new_thread(runForNMinutes, ("Thread-1", 1))
-    _thread.start_new_thread(runForNMinutes, ("Thread-2", 2))
-    _thread.start_new_thread(runForNMinutes, ("Thread-3", 3))
-    _thread.start_new_thread(runForNMinutes, ("Thread-4", 4))
-    _thread.start_new_thread(runForNMinutes, ("Thread-5", 5))
-except:
-    print ("Error")
+    if len(sys.argv) == 1 :
+        try:
+            _thread.start_new_thread(first_thread_function, ("Thread-1", 2))
+            _thread.start_new_thread(first_thread_function, ("Thread-1", 2))
+            _thread.start_new_thread(first_thread_function, ("Thread-1", 2))
+            _thread.start_new_thread(first_thread_function, ("Thread-1", 2))
+            _thread.start_new_thread(first_thread_function, ("Thread-1", 2))
+        except:
+            print ("Error 1")
+    elif len(sys.argv) == 5 :
+        try:
+            _thread.start_new_thread(second_thread_function, ("Thread-1", 2))
+            _thread.start_new_thread(second_thread_function, ("Thread-1", 2))
+            _thread.start_new_thread(second_thread_function, ("Thread-1", 2))
+        except:
+            print ("Error 2")
 
-while 1:
-    pass
-
+    time.sleep(60 * 2)
 
 if __name__ == "__main__":
     main()
